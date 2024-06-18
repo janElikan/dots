1a07bcec3a08675d1305990a4731a4f58b3cb75b
jan Elikan <janelikan@proton.me>
2024-06-18 10:33:18 +0000

    feat: make machine ids persistent
    source: https://elis.nu/blog/2020/05/nixos-tmpfs-as-root/

--- configuration.nix
+++ configuration.nix
@@ -13,6 +13,12 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
 
+  environment.etc."machine-id".source = "/nix/persist/identity/machine-id";
+  environment.etc."ssh/ssh_host_rsa_key".source = "/nix/persist/identity/ssh/ssh_host_rsa_key";
+  environment.etc."ssh/ssh_host_rsa_key.pub".source = "/nix/persist/identity/ssh/ssh_host_rsa_key.pub";
+  environment.etc."ssh/ssh_host_ed25519_key".source = "/nix/persist/identity/ssh/ssh_host_ed25519_key";
+  environment.etc."ssh/ssh_host_ed25519_key.pub".source = "/nix/persist/identity/ssh/ssh_host_ed25519_key.pub";
+
   networking.hostName = "vmnixlab0";
   networking.networkmanager.enable = true;
   networking.firewall.allowedTCPPorts = [];
