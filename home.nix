3c17287d4f4cd331e9299a059e206dd270c560c1
jan Elikan <janelikan@proton.me>
2024-06-24 08:59:34 +0000

    feat: add sway
    Configured that yesterday on the old system (now wiped), so it's a
    single commit now.

--- home.nix
+++ home.nix
@@ -3,9 +3,11 @@
 {
   home.username = "elikan";
   home.homeDirectory = "/home/elikan";
+  fonts.fontconfig.enable = true;
 
-  home.packages = [
-    # (pkgs.nerdfonts.override { fonts = [ "FantasqueSansMono" ]; })
+  home.packages = with pkgs; [
+    (nerdfonts.override { fonts = [ "FiraCode" "JetBrainsMono" ]; })
+    firefox
 
     (pkgs.writeShellScriptBin "rebuild" ''
       sudo nixos-rebuild switch --flake "/nix/persist/dots/#default"
@@ -70,6 +72,145 @@
     shellAliases = {
       lg = "lazygit";
     };
+  };
+
+  programs.kitty = {
+    enable = true;
+    font = {
+      name = "JetBrainsMono Nerd Font";
+      package = pkgs.jetbrains-mono;
+      size = 16;
+    };
+  };
+
+  wayland.windowManager.sway = {
+    enable = true;
+    config = rec {
+      modifier = "Mod4";
+      # Use kitty as default terminal
+      terminal = "kitty"; 
+
+      keybindings = {
+        "${modifier}+b" = "exec firefox";
+        "${modifier}+t" = "exec kitty";
+
+        "${modifier}+m" = "fullscreen toggle";
+        "${modifier}+q" = "kill";
+        "Mod1+Mod4+Ctrl+Shift+q" = "exec systemctl suspend";
+
+        "${modifier}+v" = "split v";
+        "${modifier}+h" = "split h";
+
+        "${modifier}+f" = "floating toggle";
+        "Mod1+Mod4+Ctrl+Shift+a" = "sticky toggle";
+
+        "${modifier}+n" = "focus left";
+        "${modifier}+e" = "focus down";
+        "${modifier}+i" = "focus up";
+        "${modifier}+o" = "focus right";
+        "${modifier}+1" = "workspace number 1";
+        "${modifier}+2" = "workspace number 2";
+        "${modifier}+3" = "workspace number 3";
+        "${modifier}+4" = "workspace number 4";
+        "${modifier}+5" = "workspace number 5";
+        "${modifier}+6" = "workspace number 6";
+        "${modifier}+7" = "workspace number 7";
+        "${modifier}+8" = "workspace number 8";
+        "${modifier}+9" = "workspace number 9";
+        "${modifier}+0" = "workspace number 10";
+
+        "${modifier}+Shift+1" = "move container to workspace number 1";
+        "${modifier}+Shift+2" = "move container to workspace number 2";
+        "${modifier}+Shift+3" = "move container to workspace number 3";
+        "${modifier}+Shift+4" = "move container to workspace number 4";
+        "${modifier}+Shift+5" = "move container to workspace number 5";
+        "${modifier}+Shift+6" = "move container to workspace number 6";
+        "${modifier}+Shift+7" = "move container to workspace number 7";
+        "${modifier}+Shift+8" = "move container to workspace number 8";
+        "${modifier}+Shift+9" = "move container to workspace number 9";
+        "${modifier}+Shift+0" = "move container to workspace number 10";
+
+      };
+
+      startup = [
+        {command = "waybar";}
+      ];
+
+      fonts = {
+        names = [ "JetBrainsMono Nerd Font" ];
+        style = "Bold";
+        size = 14.0;
+      };
+
+      colors = {
+        focused = {
+          border = "#b4befe";
+          childBorder = "#b4befe";
+          indicator = "#b4befe";
+          text = "#cdd6f4";
+          background = "#1e1e2e";
+        };
+
+        focusedInactive = {
+          border = "#1e1e2e";
+          childBorder = "#1e1e2e";
+          indicator = "#1e1e2e";
+          text = "#cdd6f4";
+          background = "#1e1e2e";
+        };
+
+        unfocused = {
+          border = "#1e1e2e";
+          childBorder = "#1e1e2e";
+          indicator = "#1e1e2e";
+          text = "#cdd6f4";
+          background = "#1e1e2e";
+        };
+
+        urgent = {
+          border = "#fab387";
+          childBorder = "#fab387";
+          indicator = "#fab387";
+          text = "#cdd6f4";
+          background = "#1e1e2e";
+        };
+      };
+
+      bars = [];
+
+      window.titlebar = false;
+    };
+  };
+
+  programs.waybar = {
+    enable = true;
+    settings = {
+      mainBar = {
+        layer = "top";
+        position = "top";
+        height = 30;
+        modules-left = [ "sway/workspaces" "sway/window" ];
+        modules-center = [ "clock" ];
+        modules-right = [ "wireplumber" "cpu" "memory" ];
+        "clock" = {
+         format = "{:%Y-%m-%dT%H:%M}";
+        };
+        "cpu" = {
+         format = " {} ";
+        };
+        "memory" = {
+         format = " {} ";
+        };
+      };
+    };
+    style = ''
+      * {
+        font-family: JetBrainsMono Nerd Font;
+        background: #1e1e2e;
+        color: #cdd6f4;
+        font-size: 22px;
+      }
+    '';
   };
 
   # Never ever change these:
